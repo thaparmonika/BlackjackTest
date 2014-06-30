85398282c8834f32c3931c838a2a5020ea5df6a5
thaparmonika <thaparmonika@yahoo.com.au>
2014-06-30 14:09:13 +1000

    Create input.php

---
+++ input.php
@@ -0,0 +1,13 @@
+<html>
+	<body>
+		<h1>Blackjack</h1>
+		<p>Choose cards.</p>
+		<form action="result.php" method="post">
+			<label for="card_1">Card 1</label>
+			<input type="text" id="card_1" name="cards[]" value="10s">
+			<label for="card_2">Card 2</label>
+			<input type="text" id="card_2" name="cards[]" value="KH">
+			<input type="submit">
+		</form>
+	</body>
+</html>
